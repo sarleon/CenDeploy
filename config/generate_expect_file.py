dcd63cc914812ab51fa54aeab21aea2a90ae57f9
sarleon <sarleon56@hotmail.com>
2016-08-08 22:27:25 -0500

    add main part

--- config/generate_expect_file.py
+++ config/generate_expect_file.py
@@ -1,8 +1,49 @@
 import os
+import commands
+import subprocess
 
+user='root'
+server='121.42.160.10'
+password='charfield123+1s'
+local_dir='/home/sarleon/daily/scptest/'
 
+server_dir='/root/'
 
+if local_dir[-1] is not '/':
+    local_dir=local_dir+'/'
+scp_expect_file_content='''
+#! /usr/bin/expect -f
+## All possible interactive messages:
+# Are you sure you want to continue connecting (yes/no)?
+# password:
+# Enter passphrase for key
+## Main
+# Delete self for secret, will not affect the following code
+file delete $argv0
+# Setup variables
+set timeout 10
+set user "%s"
+set server "%s"
+set password "%s"
+set port "22"
+# set extopt "<<EXTOPT>>"
+set scp_cmd "scp -r %s"
+set scp_opt "$user@$server:%s"
+# set scp_opt "$scp_opt $extopt"
 
+# Spawn and expect
+eval spawn $scp_cmd $scp_opt
+if {[string length $password]} {
+    expect {
+        timeout {send_user "ssh connection time out, please operate manually\n"}
+        -nocase "(yes/no)\\?" {send "yes\r"; exp_continue}
+        -nocase -re "password:|enter passphrase for key" {
+            send "$password\r"
+        }
+    }
+}
+interact
+''' %(user,server,password,local_dir,server_dir)
 
 ssh_expect_file_content='''
 #! /usr/bin/expect -f
@@ -36,6 +77,10 @@ if {[string length $password]} {
     }
 }
 interact
-''' %('asd','asd','asd')
+''' %(user,server,password)
 
 
+f=open('expect_temp_file','w')
+f.write(scp_expect_file_content)
+result=os.system('expect -f expect_temp_file')
+print result
