026178d439bd4bf71df84d24f1a5fef6f8f452d9
sarleon <sarleon@users.noreply.github.com>
2016-10-10 08:48:47 -0500

    Update main.py
    fix bug

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def run():
 if(__name__=='__main__'):
     run()
     user='root'
-    server='121.42.160.10'
-    password='charfield123+1s'
+    server='server'
+    password='pass'
     local_dir='/home/sarleon/daily/scptest/'
     server_dir='/root/'
     generate_expect(user,server,password,local_dir,server_dir)
